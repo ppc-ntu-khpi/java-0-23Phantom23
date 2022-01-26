c66358b060f4a5eef4acfd9ed32e8f7deac79273
23Phantom23 <91052918+23Phantom23@users.noreply.github.com>
2022-01-26 23:19:11 +0200

    Delete src directory

--- src/Quotation.java
+++
@@ -1,6 +0,0 @@
-public class Quotation {
-  String quote = "The difference between ordinary and extraordinary is that little extra.";
-  public void display() {
-    System.out.println(quote);
-  }
-}
