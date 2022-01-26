297331beaf7efed2df58ff37d6d63e8abf12b379
23Phantom23 <91052918+23Phantom23@users.noreply.github.com>
2022-01-26 23:22:10 +0200

    Add files via upload

---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+  String quote = "Success consists of going from failure to failure without loss of enthusiasm";
+  String author = "Winston Churchill";
+  public void display() {
+    System.out.println(quote);
+    System.out.println("(" + author + ")");
+  }
+}
